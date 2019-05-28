76160d93dced2a8cc7e262d5f3e30293bbb61ce2
morrise0316 <eli.h.morris@gmail.com>
2019-05-28 11:57:11 -0700

    SQLNotes stuff

---
+++ app/src/main/java/com/example/mycontactapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mycontactapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
+        Log.d("MyContactApp", "MainActivity: instantiated DatabaseeHelper");
+    }
+}
