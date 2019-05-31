824ec3e4638677fcd4c0d033e4b007df6c48dad0
morrise0316 <eli.h.morris@gmail.com>
2019-05-31 12:27:04 -0700

    SQLite Stuff Finished
    Finished the initial requirements for the SQLite project.

--- app/src/main/java/com/example/mycontactapp/DatabaseHelper.java
+++ app/src/main/java/com/example/mycontactapp/DatabaseHelper.java
@@ -1,35 +1,92 @@
 package com.example.mycontactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.widget.Toast;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "Contact2019.db";
-    public static final String TABLE_NAME = "Contact2019_table";
+    public static final String TABLE_NAME = "Contact2019_2";
     public static final String ID = "ID";
     public static final String COLUMN_NAME_CONTACT = "contact";
-
+    public static final String COLUMN_NAME_BIRTHDAY = "birthday";
+    public static final String COLUMN_NAME_FAVORITE_COLOR = "favorite_color";
     public static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + TABLE_NAME + " (" + ID  + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    COLUMN_NAME_CONTACT + " TEXT)";
-
+            "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" + ID  + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    COLUMN_NAME_CONTACT + " TEXT," + COLUMN_NAME_BIRTHDAY + " TEXT," + COLUMN_NAME_FAVORITE_COLOR
+            + " TEXT)";
+    public static final String SQL_RESET_PRIMARY_KEY = "DELETE FROM SQLITE_SEQUENCE WHERE NAME=" + "'"+TABLE_NAME+"'";
+    public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    public static final String SQL_CLEAR_TABLE = "DELETE FROM " + TABLE_NAME;
 
     public DatabaseHelper(Context context) {
         super(context,DATABASE_NAME, null, DATABASE_VERSION);
-        SQLiteDatabase db = this.getWritableDatabase(); //FOR TEST ONLY - WILL REMOVE LATER
-        Log.d("MyContactApp","DatabaseeHelper: constructed DatabaseHelper" );
+       SQLiteDatabase db = this.getWritableDatabase(); //FOR TEST ONLY - WILL REMOVE LATER
+        Log.d("MyContactApp","DatabaseHelper: constructed DatabaseHelper" );
+       onCreate(db);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        Log.d("MyContactApp","DatabaseHelper: creating DatabaseHelper" );
+        db.execSQL(SQL_CREATE_ENTRIES);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.d("MyContactApp","DatabaseHelper: upgrading DatabaseHelper" );
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
 
+    public boolean insertData(String name, String birthday, String favColor) {
+        Log.d("MyContactApp", "DatabaseHelper: inserting data");
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = this.getAllData();
+        while(res.moveToNext())
+        {
+            if (res.getString(1).equals(name) && res.getString(2).equals(birthday) && res.getString(3).equals(favColor)) {
+                Log.d("MyContactApp", "DatabaseHelper: Same contact attempted to be inserted twice");
+                return false;
+            }
+
+
+
+        }
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_NAME_CONTACT, name);
+        contentValues.put(COLUMN_NAME_BIRTHDAY,birthday);
+        contentValues.put(COLUMN_NAME_FAVORITE_COLOR, favColor);
+
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1) {
+           Log.d("MyContactApp", "DatabaseHelper: Contact insert FAILED");
+           return false;
+        }
+        else {
+            Log.d("MyContactApp", "DatabaseHelper: Contact insert PASSED");
+            return true;
+        }
+
+    }
+
+    public void clearData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL(SQL_CLEAR_TABLE);
+        db.execSQL(SQL_RESET_PRIMARY_KEY);
+
+    }
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
     }
 }
--- app/src/main/java/com/example/mycontactapp/MainActivity.java
+++ app/src/main/java/com/example/mycontactapp/MainActivity.java
@@ -1,17 +1,103 @@
 package com.example.mycontactapp;
 
+import android.app.AlertDialog;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDb;
+    EditText editName;
+    EditText editBirthday;
+    EditText editFavColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editName = findViewById(R.id.editText_name);
+        editBirthday = findViewById(R.id.editText_birthday);
+        editFavColor = findViewById(R.id.editText_FavColor);
+
         myDb = new DatabaseHelper(this);
-        Log.d("MyContactApp", "MainActivity: instantiated DatabaseeHelper");
+        Log.d("MyContactApp", "MainActivity: instantiated DatabaseHelper");
+        ((Button)findViewById(R.id.seeTable)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewData(v);
+            }
+        });
+    }
+
+    public void addData (View view) {
+        boolean isInserted = myDb.insertData(editName.getText().toString(),editBirthday.getText().toString(),
+                editFavColor.getText().toString());
+
+        if(isInserted == true) {
+            Toast.makeText(MainActivity.this,"Success - contact inserted",Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(MainActivity.this, "FAILED - contact not inserted or already exists", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void viewData(View view) {
+        Cursor res = myDb.getAllData();
+
+        if(res.getCount() == 0) {
+            showMessage("Error", "No contact data found in the db");
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()) {
+            //append res column 0,.... to the buffer - see StringBuffer and Cursor api's for reference
+            buffer.append("ID: " + res.getString(0) + "\n");
+            buffer.append("Name: " + res.getString(1) + "\n");
+            buffer.append("Birthday: " + res.getString(2) + "\n");
+            buffer.append("Fav Color: " + res.getString(3) + "\n");
+        }
+
+        showMessage("Data", buffer.toString());
+    }
+
+    public void findData (View view) {
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0) {
+            showMessage("Error", "No contact data found in the db");
+            return;
+        }
+        StringBuffer buffer = new StringBuffer();
+        while(res.moveToNext()) {
+            if(editName.getText().toString().equals(res.getString(1)) ||
+            editBirthday.getText().toString().equals(res.getString(2)) ||
+            editFavColor.getText().toString().equals(res.getString(3)))
+            {
+                buffer.append("ID: " + res.getString(0) + "\n");
+                buffer.append("Name: " + res.getString(1) + "\n");
+                buffer.append("Birthday: " + res.getString(2) + "\n");
+                buffer.append("Fav Color: " + res.getString(3) + "\n");
+            }
+        }
+        showMessage("Matching Data",buffer.toString());
+    }
+
+    public void clearData(View view){
+        myDb.clearData();
+    }
+
+    public void showMessage (String title , String message) {
+        //put Log.d's in here
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
